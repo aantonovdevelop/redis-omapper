cd8673ed3a320f2cbdf4c6276f84ce55fd897758
Artem Antonov <aantonov.develop@gmail.com>
2016-03-29 15:38:15 +0500

    Implement package exports

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var model_factory = require('./src/model-factory');
+var Repository = require('./src/repository');
+
+module.exports = {
+    model_factory: model_factory,
+    Repository: Repository
+};
