4b611b89ea67509a2b49a2686e1a84a3a8cd5acf
Artem Antonov <aantonov.develop@gmail.com>
2016-04-07 13:20:04 +0500

    Fix keys exports

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 var model_factory = require('./src/model-factory');
 var Repository = require('./src/repository');
+var keys = require('./src/foreign-keys');
 
 module.exports = {
     model_factory: model_factory,
+    keys: keys,
     Repository: Repository
 };
